3cda2423499af20973e8bbb74f98783e8e04d9e7
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-15 15:22:39 +0100

    it1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+  const contactsDB = contacts.slice(1, 6);
+console.log(contactsDB)
   return (
     <div className="App">
-      
-      
+      <table>
+        <tr>
+          <td>Picture</td>
+          <td>Name</td>
+          <td>Popularity</td>
+        </tr>
+        {contactsDB.map((contact) => {
+          const pop = contact.popularity;
+        return (
+          <tr>
+            <td><img width="50px" src={contact.pictureUrl} className="profile" alt="profile"/></td>
+            <td>{contact.name}</td>
+            <td>{pop.toFixed(2)}</td>
+          </tr>
+        );
+      })}
+      </table>
     </div>
   );
 }
