0fb39fcf70093abb1a9474824258638bcf6e6f2a
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-15 15:53:39 +0100

    it2 done

--- src/App.js
+++ src/App.js
@@ -11,14 +11,28 @@ console.log(contactsDB)
           <td>Picture</td>
           <td>Name</td>
           <td>Popularity</td>
+          <td>won Oscar</td>
+          <td>Won Emmy</td>
         </tr>
         {contactsDB.map((contact) => {
           const pop = contact.popularity;
+          let wonOscar = "";
+          let wonEmmy = "";
+          if(contact.wonOscar)
+          {
+            wonOscar += '🏆';
+          }
+          if(contact.wonEmmy)
+          {
+            wonEmmy += '🏆';
+          }
         return (
           <tr>
-            <td><img width="50px" src={contact.pictureUrl} className="profile" alt="profile"/></td>
+            <td><img width="50px" src={contact.pictureUrl}/></td>
             <td>{contact.name}</td>
             <td>{pop.toFixed(2)}</td>
+            <td>{wonOscar}</td>
+            <td>{wonEmmy}</td>
           </tr>
         );
       })}
